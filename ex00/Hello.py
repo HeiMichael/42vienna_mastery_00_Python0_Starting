e44df4e97abad062880a584b54b00dcf5e4f6fc5
miheider <you@42.com>
2025-10-11 17:27:02 +0200

    ex00 done

--- ex00/Hello.py
+++ ex00/Hello.py
@@ -1,9 +1,29 @@
 ft_list = ["Hello", "tata!"]
 ft_tuple = ("Hello", "toto!")
-ft_set = {"Hello", "tutu"}
-ft_dict = {"Hello" : "titi"}
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello" : "titi!"}
 
-#my code here
+#replace "tata!" with "World"
+ft_list = ft_list[:1]
+ft_list.append("World")
+
+# alternatively:
+#ft_list[1] = "World"
+
+#replace "toto!" with "Austria"
+#tuple does not have a .append() or .remove() method
+temp = list(ft_tuple)
+temp = temp[:1]
+temp.append("Austria")
+ft_tuple = tuple(temp)
+
+#replace "tutu!" with "Vienna"
+ft_set.discard("tutu!")
+ft_set.add("Vienna")
+sorted(ft_set)
+
+#replace "titi!" with "42Vienna"
+ft_dict["Hello"] = "42vienna"
 
 print(ft_list)
 print(ft_tuple)
