ce5661883916f25290cb1b877fc87e31848635ac
miheider <miheider@student.42vienna.com>
2025-10-13 17:58:05 +0200

    started with ex01

---
+++ ex01/format_ft_time.py
@@ -0,0 +1,8 @@
+from time import time
+from datetime import datetime, timezone
+
+seconds = int(time())
+print(f"Seconds since January 1, 1970: {seconds} or {seconds:.3e} in scientific notation")
+
+seconds_alt = int(datetime.now(timezone.utc).timestamp())
+print(f"Seconds since January 1, 1970: {seconds_alt} or {seconds_alt:.3e} in scientific notation")
